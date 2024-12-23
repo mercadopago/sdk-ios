028c58e679c239c23f3d5476de4b52efe885fb8b
Guilherme Prata Costa <guilherme.pcosta@mercadolivre.com>
2024-12-23 18:46:46 -0300

    test without plugin

--- Package.swift
+++ Package.swift
@@ -16,17 +16,12 @@ let package = Package(
         )
     ],
     dependencies: [
-        // SwiftLint plugin dependency
-        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.57.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CoreMethods",
-            plugins: [
-                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
-            ]
+            name: "CoreMethods"
         ),
         .testTarget(
             name: "CoreMethodsTests",
