ad9164e3a4327eb34847ea619b7afd789c9dcbfd
Guilherme Prata Costa <guilherme.pcosta@mercadolivre.com>
2024-12-26 11:08:24 -0300

    put swiftlint inside example build phase

--- Package.swift
+++ Package.swift
@@ -16,18 +16,11 @@ let package = Package(
             targets: ["CoreMethods"]
         )
     ],
-    dependencies: [
-        // SwiftLint plugin dependency
-        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.57.1")
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CoreMethods",
-            plugins: ProcessInfo.processInfo.environment["CI"] == nil ? [
-                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
-            ] : []
+            name: "CoreMethods"
         ),
         .testTarget(
             name: "CoreMethodsTests",
