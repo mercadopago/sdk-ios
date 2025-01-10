2fc37d0d34a28197aac1b9cf9eb295c8dc18146f
Guilherme Prata Costa <guilherme.pcosta@mercadolivre.com>
2025-01-10 11:52:55 -0300

    feature: create pci field state
    + unit tests and snapshot

---
+++ Sources/CoreMethods/Extensions/String+ApplyMask.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+package extension String {
+    func applyMask(_ pattern: String, separator: Character) -> String {
+        let numbers = onlyNumbers()
+        var result = ""
+        var index = numbers.startIndex
+
+        for char in pattern {
+            guard index < numbers.endIndex else { break }
+
+            if char == "#" {
+                result.append(numbers[index])
+                index = numbers.index(after: index)
+            } else {
+                result.append(separator)
+            }
+        }
+
+        return result
+    }
+}
---
+++ Sources/CoreMethods/Extensions/String+OnlyNumbers.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  MercadoPagoSDK-iOS
+//
+//  Created by Guilherme Prata Costa on 08/01/25.
+//
+import Foundation
+
+package extension String {
+    func onlyNumbers() -> String {
+        components(separatedBy: CharacterSet.decimalDigits.inverted)
+            .joined()
+    }
+}
