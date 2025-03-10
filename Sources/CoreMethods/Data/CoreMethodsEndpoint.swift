4db8ea964ffcff2258178b524d5e9cc9691d548c
Guilherme Prata Costa <guilherme.pcosta@mercadolivre.com>
2025-03-10 17:13:46 -0300

    remove product id example

--- Sources/CoreMethods/Data/CoreMethodsEndpoint.swift
+++ Sources/CoreMethods/Data/CoreMethodsEndpoint.swift
@@ -12,7 +12,7 @@ private enum Constants {
     static let baseURLToken = "https://api.mercadopago.com"
     static let baseURLBricks = "https://api.mercadopago.com/cho-off/beta"
 
-    static let productID = "CHQBUNESFQCVF58JFECG"
+    static let productID = ""
 }
 
 /// Endpoints
