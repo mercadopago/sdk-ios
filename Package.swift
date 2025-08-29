4b2c090dd78ac775050d427dfc8e69c3257196d0
Guilherme Prata Costa <guilherme.pcosta@mercadolivre.com>
2025-08-29 13:55:07 -0300

    remove library apple pay

--- Package.swift
+++ Package.swift
@@ -14,11 +14,6 @@ let package = Package(
         .library(
             name: "CoreMethods",
             targets: ["CoreMethods"]
-        ),
-        
-        .library(
-            name: "MPApplePay",
-            targets: ["MPApplePay"]
         )
     ],
     dependencies: [
