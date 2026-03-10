c61da6e0c1be48d7c65d782ea7ca200eea31e777
Danielle Nozaki Ogawa <danielle.ogawa+meli@mercadolivre.com>
2026-03-10 14:31:39 -0300

    created MPFixedFooterButtonData aligned with android

--- Sources/MPComponents/BaseElements/Footer/MPFooter.swift
+++ Sources/MPComponents/BaseElements/Footer/MPFooter.swift
@@ -5,8 +5,8 @@
 //  Created by Guilherme Prata Costa on 24/11/25.
 //
 
-import SwiftUI
 import MPFoundation
+import SwiftUI
 
 /// A footer component that displays payment summary information such as total amount and payment method details.
 /// ## Usage
@@ -24,22 +24,20 @@ import MPFoundation
 /// ```
 ///
 package struct MPFooter: View {
-    
     // MARK: - Properties
-    
+
     private let title: String
     private let amount: MPAmountData?
     private let subtitle: String?
-    private let buttonLabel: String?
-    private let action: (() -> Void)?
-    
+    private let buttonData: MPFixedFooterButtonData?
+
     // MARK: - Environment
-    
+
     @Environment(\.mpFooterStyle) private var style: any MPFooterStyle
     @Environment(\.checkoutTheme) var theme: MPTheme
-    
+
     // MARK: - Initialization
-    
+
     /// Creates a new footer with the specified configuration.
     ///
     /// - Parameters:
@@ -50,16 +48,14 @@ package struct MPFooter: View {
         title: String = String(),
         amount: MPAmountData? = nil,
         subtitle: String? = nil,
-        buttonLabel: String,
-        action: @escaping () -> Void
+        buttonData: MPFixedFooterButtonData? = nil
     ) {
         self.title = title
         self.amount = amount
         self.subtitle = subtitle
-        self.buttonLabel = buttonLabel
-        self.action = action
+        self.buttonData = buttonData
     }
-    
+
     package init(
         title: String,
         amount: MPAmountData? = nil,
@@ -68,12 +64,11 @@ package struct MPFooter: View {
         self.title = title
         self.amount = amount
         self.subtitle = description
-        self.buttonLabel = nil
-        self.action = nil
+        self.buttonData = nil
     }
-    
+
     // MARK: - Body
-    
+
     package var body: some View {
         let configuration = MPFooterStyleConfiguration(
             summaryLine: summaryLineView,
@@ -81,75 +76,76 @@ package struct MPFooter: View {
             button: button,
             hasDescription: subtitle != nil
         )
-        
+
         return AnyView(
-            style.resolve(configuration: configuration)
+            self.style.resolve(configuration: configuration)
         )
     }
-    
+
     // MARK: - Summary Line View
-    
+
     @ViewBuilder
     private var summaryLineView: some View {
         if let amount {
-            HStack(alignment: .center, spacing: theme.spacings.xtiny) {
+            HStack(alignment: .center, spacing: self.theme.spacings.xtiny) {
                 // Label
-                Text(title)
+                Text(self.title)
                     .textStyle(.largeEmphasis())
                     .lineLimit(1)
-                
+
                 Spacer()
-                
+
                 // Amount
-                amountView
+                self.amountView
             }
             .accessibilityElement(children: .ignore)
-            .accessibility(label: Text("\(title) \(amount)"))
+            .accessibility(label: Text(self.createAccessibilityLabel()))
         }
     }
-    
+
     // MARK: - Description Line View
-    
+
     @ViewBuilder
     private var descriptionLineView: some View {
         if let descriptionText = subtitle {
             HStack {
                 Spacer()
-                
+
                 Text(descriptionText)
                     .textStyle(.bodyMedium(colorType: .secondary))
                     .lineLimit(1)
             }
         }
     }
-    
+
     @ViewBuilder
     private var button: some View {
-        if let action, let buttonLabel {
+        if let buttonData {
             Button {
-                action()
+                buttonData.onClick()
             } label: {
-                Text(buttonLabel)
+                Text(buttonData.text)
             }
+            .mpButtonStyle(variant: buttonData.style)
         }
     }
-    
+
     @ViewBuilder
     private var amountView: some View {
         if let amount {
-            HStack(alignment: .top, spacing: theme.spacings.xnano) {
+            HStack(alignment: .top, spacing: self.theme.spacings.xnano) {
                 Text(amount.currencySymbol)
                     .textStyle(.largeEmphasis())
-                    .foregroundColor(theme.colors.text.primary)
+                    .foregroundColor(self.theme.colors.text.primary)
                     .lineLimit(1)
                 Text(amount.integerPart)
                     .textStyle(.largeEmphasis())
-                    .foregroundColor(theme.colors.text.primary)
+                    .foregroundColor(self.theme.colors.text.primary)
                     .lineLimit(1)
                 VStack(alignment: .leading) {
                     Text(amount.decimalPart)
                         .textStyle(.smallMediumEmphasis())
-                        .foregroundColor(theme.colors.text.primary)
+                        .foregroundColor(self.theme.colors.text.primary)
                         .lineLimit(1)
                     Spacer()
                 }
@@ -157,46 +153,59 @@ package struct MPFooter: View {
             }
         }
     }
+
+    private func createAccessibilityLabel() -> String {
+        return "\(self.title) \(self.amount?.currencySymbol ?? "") \(self.amount?.integerPart ?? ""), \(self.amount?.decimalPart ?? "")"
+    }
 }
 
 // MARK: - Preview
 
 #if DEBUG
-struct MPFooter_Previews: PreviewProvider {
-    static var previews: some View {
-        VStack(spacing: 20) {
-            Spacer()
-            
-            // Footer with description
-            MPFooter(
-                title: "Total",
-                amount: .init(currencySymbol: "R$", integerPart: "500", decimalPart: "00"),
-                subtitle: "Santander Crédito **** 4561",
-                buttonLabel: MPStrings.CardForm.button,
-                action: {
-                    print("button action")
-                }
-            )
-            .disabled(true)
-            
-            // Footer without description
-            MPFooter(
-                title: "Total",
-                amount: .init(currencySymbol: "R$", integerPart: "1.250", decimalPart: "00"),
-                buttonLabel: MPStrings.CardForm.button,
-                action: {
-                    print("button action")
-                }
-            )
-        }
-        .loadMPFonts()
-    }
-}
-#endif
+    struct MPFooter_Previews: PreviewProvider {
+        static var previews: some View {
+            VStack(spacing: 20) {
+                Spacer()
 
+                // Footer with description
+                MPFooter(
+                    title: "Total",
+                    amount: .init(currencySymbol: "R$", integerPart: "500", decimalPart: "00"),
+                    subtitle: "Santander Crédito **** 4561",
+                    buttonData: .init(
+                        text: MPStrings.CardForm.button,
+                        onClick: {
+                            print("button action")
+                        }
+                    )
+                )
+                .disabled(true)
+
+                // Footer without description
+                MPFooter(
+                    title: "Total",
+                    amount: .init(currencySymbol: "R$", integerPart: "1.250", decimalPart: "00"),
+                    buttonData: .init(
+                        text: MPStrings.CardForm.button,
+                        onClick: {
+                            print("button action")
+                        }
+                    )
+                )
+            }
+            .loadMPFonts()
+        }
+    }
+#endif
 
 package struct MPAmountData {
     var currencySymbol: String
     var integerPart: String
     var decimalPart: String
+}
+
+package struct MPFixedFooterButtonData {
+    var text: String
+    var style: MPButtonStyle.Variant = .loud
+    var onClick: () -> Void
 }
