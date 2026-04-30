72b9d83756cd69c7068c999bb6101107e272754f
Danielle Nozaki Ogawa <danielle.ogawa+meli@mercadolivre.com>
2026-04-30 15:16:56 -0300

    removed from products

--- Package.swift
+++ Package.swift
@@ -21,10 +21,6 @@ let package = Package(
             targets: ["MPApplePay"]
         ),
         .library(
-            name: "MercadoPagoCheckout",
-            targets: ["MercadoPagoCheckout"]
-        ),
-         .library(
             name: "MPExtended",
             targets: ["MPExtended"]
         )
