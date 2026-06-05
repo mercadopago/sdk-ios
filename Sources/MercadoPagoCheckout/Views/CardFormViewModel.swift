1d754887ef1d337abbe3ca54d7a9feb0c5595ccc
daniellemeli <danielle.ogawa@mercadolivre.com>
2026-06-05 13:06:48 -0300

    [CHOBK-3983] (Feature): Add Melidata tracks for order flow (#231)
    * [Feature] Checkout Card Payment Installments
    
    * [CHOBK-4087] (Feature): Integrate order processing in checkout flow (#230)
    
    * [Feature] Checkout Card Payment Installments
    
    * [CHOBK-3979] (Feature): Order Transaction Service (#226)
    
    * [Feature] Checkout Card Payment Installments
    
    * [CHOBK-3979] (Feature): Order Transaction Service
    
    ---------
    
    Co-authored-by: Mirror Bot <mirror-bot@users.noreply.github.com>
    
    * [CHOBK-4087] (Feature): Integrate order processing in checkout flow
    
    ---------
    
    Co-authored-by: Mirror Bot <mirror-bot@users.noreply.github.com>
    
    * [CHOBK-4128] (Feature): Add min and max installments support (#219)
    
    * [Feature] Checkout Card Payment Installments
    
    * fix: add MercadoPagoCheckout library product to Package.swift
    
    * revert: remove MercadoPagoCheckout product from Package.swift
    
    * [CHOBK-4128] (Feature): Add min and max installments support
    
    * mirror mainlistview
    
    * fix tests
    
    * fix result test
    
    ---------
    
    Co-authored-by: Mirror Bot <mirror-bot@users.noreply.github.com>
    Co-authored-by: Guilherme Prata Costa <guilherme.pcosta@mercadolivre.com>
    
    * [CHOBK-3983] (Feature): Add Melidata tracks for order flow
    
    * fix mirror
    
    ---------
    
    Co-authored-by: Mirror Bot <mirror-bot@users.noreply.github.com>
    Co-authored-by: Guilherme Prata Costa <guilherme.pcosta@mercadolivre.com>

--- Sources/MercadoPagoCheckout/Views/CardFormViewModel.swift
+++ Sources/MercadoPagoCheckout/Views/CardFormViewModel.swift
@@ -153,7 +153,7 @@ final class CardFormViewModel: ObservableObject {
         guard
             self.config.amount != .zero
         else {
-            return nil 
+            return nil
         }
         return MPAmountData(from: self.config.amount, currencySymbol: self.currencySymbol)
     }
@@ -328,7 +328,6 @@ final class CardFormViewModel: ObservableObject {
             onFailure(error)
         }
     }
-
 }
 
 // MARK: - Analytics
